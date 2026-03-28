63b70d687059fbe29494597eb1892c623bae55e3
y23d17 <mjfumiten@antiquespride.edu.ph>
2026-03-28 23:08:06 +0800

    feat: added app bottom bar

---
+++ src/components/shared/BottomBar.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Lessons from '@/lib/assets/images/lesson.png';
+	import Games from '@/lib/assets/images/games.png';
+	import Books from '@/lib/assets/images/books.png';
+</script>
+
+<nav class="bottom">
+	<a href="/home">
+		<i>home</i>
+		<div>Home</div>
+	</a>
+
+	<a href="/lessons">
+		<button class="circle transparent">
+			<img src={Lessons} alt="lessons" />
+		</button>
+
+		<div>Lessons</div>
+	</a>
+
+	<a href="/games">
+		<button class="circle transparent">
+			<img src={Games} alt="lessons" />
+		</button>
+
+		<div>Games</div>
+	</a>
+
+	<a href="/library">
+		<button class="circle transparent">
+			<img src={Books} alt="lessons" />
+		</button>
+
+		<div>Library</div>
+	</a>
+</nav>
