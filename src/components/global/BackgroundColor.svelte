c996bae822fec9bb385a43db95e5c34a9c6a43c5
y23d17 <mjfumiten@antiquespride.edu.ph>
2026-03-28 22:41:41 +0800

    chore: added background color globals

---
+++ src/components/global/BackgroundColor.svelte
@@ -0,0 +1,17 @@
+<style>
+	:global(.bg-color-ice-cream) {
+		background: linear-gradient(90deg, #ff9a8b 0%, #ff6a88 55%, #ff99ac 100%);
+	}
+
+	:global(.bg-color-lavender) {
+		background: linear-gradient(180deg, #ffd34d 0%, #f9a13d 100%);
+	}
+
+	:global(.bg-color-sky) {
+		background: linear-gradient(180deg, #81d4fa 0%, #4fc3f7 100%);
+	}
+
+	:global(.bg-color-sunny) {
+		background: linear-gradient(180deg, #d1c4e9 0%, #b39ddb 100%);
+	}
+</style>
---
+++ src/components/shared/MenuCard.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { image, imageAlt, title, translated, class: className, ...props } = $props();
+</script>
+
+<article class="s12 center-align {className}" {...props}>
+	<img src={image} alt={imageAlt} />
+	<h5>{title}</h5>
+	<em>({translated})</em>
+</article>
